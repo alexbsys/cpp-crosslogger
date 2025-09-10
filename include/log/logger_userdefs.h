d62d3582f51e38d67b1e5c12ea403dd0db055abb
alexbsys <alex.bobryshev.ua@gmail.com>
2025-09-10 20:50:22 +0300

    add MUSL support (openwrt)

--- include/log/logger_userdefs.h
+++ include/log/logger_userdefs.h
@@ -84,13 +84,18 @@
 #define LOG_SET_CONFIG_PARAM(name,value)
 #define LOG_GET_CONFIG_PARAM(name, buf, size)
 #define LOG_FLUSH()
+#define LOG_SHUTDOWN()
 
+#define LOG_TEXT(v,...)
+#define LOG_CMD(...)
+#define LOG_GET_LOGGER()
+#define LOG_STREAM(v)
 
 #else  /*LOG_ENABLED*/
 
 #if LOG_USE_MODULEDEFINITION
 #define LOG_GET_CALLER_ADDR logging_get_caller_address()
-#else
+#else //LOG_USE_MODULEDEFINITION
 #define LOG_GET_CALLER_ADDR 0L
 #endif  // LOG_USE_MODULEDEFINITION
 
@@ -133,7 +138,10 @@
   (logobj)->get_config_param(name, (buf),(size))
 
 #define LOGOBJ_FLUSH(logobj) \
-  (logobj)->flush()
+  (logobj)->flush(1)
+
+#define LOGOBJ_SHUTDOWN() (logging::_logger->release())
+
 
 #define LOGOBJ_GET_DEFAULT_LOGGER() (logging::_logger->get())
 
@@ -142,7 +150,10 @@
   LOGOBJ_GET_DEFAULT_LOGGER()->stream((v), LOG_GET_CALLER_ADDR, \
       __FUNCTION__, __FILE__, __LINE__)
 
-#else
+#define LOG_GET_LOGGER() \
+  LOGOBJ_GET_DEFAULT_LOGGER()
+
+#else //defined(LOG_CPP) && (!LOG_USE_DLL || defined(LOG_THIS_IS_DLL))
 // Logger outproc implementation
 
 #define LOGOBJ_CMD(logobj,cmdid,v,p,l) \
@@ -152,7 +163,6 @@
 #define LOGOBJ_TEXT(logobj, v, ...) \
   __c_logger_log((logobj), (v), LOG_GET_CALLER_ADDR, __FUNCTION__, \
     __FILE__, __LINE__, __VA_ARGS__)
-
 
 #define LOGOBJ_RELOAD_CONFIG(logobj) \
   __c_logger_reload_config((logobj))
